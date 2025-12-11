59a208ff07a908902b7d03d2434a106c604264c5
lauraschmied024-droid <lauraschmied024@gmail.com>
2025-12-11 14:14:14 +0100

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0dc86cc81141760b"; }
+/******/ 		__webpack_require__.h = function() { return "b11fc2a2e914e525"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
